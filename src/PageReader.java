36471a3533a42127951641135a7cc409a6f7c2e4
hankliu5 <liu19901124@gmail.com>
2016-03-11 10:39:06 -0800

    shows users current grades and their rank now.

--- src/PageReader.java
+++ src/PageReader.java
@@ -11,22 +11,31 @@ public class PageReader {
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
     String link = "http://www.gradesource.com/reports/5108/27681/coursestand.html";
+    Scanner scnr = new Scanner(System.in);
     try {
-  		Scanner scnr = new Scanner(System.in);
-  		System.out.print("Please enter your secret number: ");
-  		int userInput = scnr.nextInt();
       Document doc = Jsoup.connect(link).get();
       Elements tableElements = doc.select("table");
       Elements tableRowElements = tableElements.select(":not(thead) tr");
+      int[] secretNums = new int[tableRowElements.size()];
+      int rank = -1;
       
+  		System.out.print("Please enter your secret number: ");
+  		int userInput = scnr.nextInt();
+      
+  		int rowWithData = 0;
       for (int i = 0; i < tableRowElements.size(); i++) {
         Element row = tableRowElements.get(i);
         Elements rowItems = row.select("td");
         if (rowItems.size() > 2) { // 2 is for filtering the upper title, link or images
         	if (rowItems.get(0).text().length() == 4) {
+        		secretNums[rowWithData] = Integer.parseInt(rowItems.get(0).text()); 
+        		if (secretNums[rowWithData] == userInput) {
+        			rank = rowWithData + 1;
+        		}
+        		rowWithData++;
         		if (userInput == Integer.parseInt(rowItems.get(0).text())) {
               for (int j = 0; j < rowItems.size(); j++) {
-              	System.out.print(rowItems.get(j).text() + " ");
+                System.out.print(rowItems.get(j).text() + " ");   
               }
               System.out.println();
         		}
@@ -34,8 +43,12 @@ public class PageReader {
         }
       }
 
+      System.out.println("");
+  		System.out.println("Your current rank is " + rank);
    } catch (IOException e) {
       e.printStackTrace();
+   } finally {
+  	 scnr.close();
    }
 	}
 }
